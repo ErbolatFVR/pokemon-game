eb268e2fe030455e6d786b3dd34c3ce9816a2e68
ErbolatFVR <57899879+ErbolatFVR@users.noreply.github.com>
2021-01-27 19:13:53 +0600

    homework-2
    Можно видео про Гит? как работать с консолю и связать репозитори аккаунт и.т,д?

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const element = (
+    <>
+        <h1> Hello world </h1>
+        <h1> Hello world </h1>
+    </>
 );
+
+ReactDOM.render(element, document.getElementById('root'));
