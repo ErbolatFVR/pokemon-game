5af25543b533c37fb447e0f4e997cda9cc3221ef
ErbolatFVR <57899879+ErbolatFVR@users.noreply.github.com>
2021-01-29 00:29:08 +0600

    HomeWork-2
    Закончил 2ой ДЗ

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,16 @@
 import style from './Layout.module.css'
 
-const Layout = ({id, title, descr, urlBg, colorBg}) => {
-    return (
-        <section className={style.root} id={Layout} style={backgroundImage:url({bg1})}>
+const Layout = ({id, title, descr, bgColor, bgImage}) => {
+return (
+        <section className={style.root} id={Layout} style={{backgroundImage: bgImage ? `url(${bgImage})` : 'none',
+            backgroundColor: bgColor}}>
             <div className={style.wrapper}>
                 <article>
                     <div className={style.title}>
-                        <h3>
-                            {title}
-                        </h3>
-                        <span
-                            className={style.desc + ' ' + style.full}
-                        />
+                        <h3>{title}</h3>
                     </div>
                     <div className={style.desc + ' ' + style.full}>
-                        <p>
-                            {descr}
-                        </p>
+                        <p>{descr}</p>
                     </div>
                 </article>
             </div>
